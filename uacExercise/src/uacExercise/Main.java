5434014946439377a4cbd90b1867a82a31873096
Akbar Eka Putra <akbarekaputra01@gmail.com>
2025-05-22 13:28:38 +0700

    Test

--- uacExercise/src/uacExercise/Main.java
+++ uacExercise/src/uacExercise/Main.java
@@ -2,7 +2,7 @@ package uacExercise;
 
 public class Main {
 	public Main() {
-		System.out.println("Hello World");
+		System.out.println("Hello World 2");
 	}
 	
 	public static void main(String[] args) {
